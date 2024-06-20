f45088074c13dfd312100ef595f5e323c98d3df1
jjaeho0415 <jjh0773@naver.com>
2024-06-20 18:04:41 +0900

    Refactor: 메인페이지 퍼블리싱 작업중

--- src/app/home/_component/HomeVoted.tsx
+++ src/app/home/_component/HomeVoted.tsx
@@ -1,13 +1,14 @@
-import PostVotingChampList from "@/app/post/_component/PostVotingChampList";
-import PostVotingGraph from "@/app/post/_component/PostVotingGraph";
+import PostVotingChampList from '@/app/post/_component/PostVotingChampList';
+import { inGameInfo } from '@/app/post/_component/VoteResult';
+import DoughnutChart from '@/components/DoughnutChart';
 
 export default function HomeVoted() {
   return (
     <>
-      <div className="h-[8.563rm] rounded-[1.875em] bg-gray-100">
-        <div className="flex items-center p-[1.563em] text-[0.625em]">
+      <div className='flex h-full rounded-[1.875em] items-center'>
+        <div className='flex items-center justify-center p-[1rem] text-[0.625em] gap-[30px]'>
           <PostVotingChampList />
-          <PostVotingGraph />
+          <DoughnutChart ingameInfos={inGameInfo} />
         </div>
       </div>
     </>
