bc51e10fafe61130aa84d1975a8d44c49fef87fc
shuding0307 <suhyun2116@gmail.com>
2024-06-26 01:11:59 +0900

    fix

--- src/api/getPostList.ts
+++ src/api/getPostList.ts
@@ -1,7 +1,7 @@
 import api from "@/_lib/fetcher";
 
 export default async function getPostList(orderBy: string, keyword: string) {
-  const data  = await api.get({ endpoint: `/api/post?orderby=${orderBy}&keyword=${keyword}` });
+  const data  = await api.get({ endpoint: `/post?orderby=${orderBy}&keyword=${keyword}` });
 
   return data;
 }
