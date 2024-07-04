633b16413f591764a86da90817659297267197aa
shuding0307 <suhyun2116@gmail.com>
2024-07-04 21:18:01 +0900

    style: myPage publishing

--- src/components/BarChart.tsx
+++ src/components/BarChart.tsx
@@ -7,13 +7,13 @@ interface BarChartProps {
 
 const BarChart: React.FC<BarChartProps> = ({ num }) => {
   return (
-    <div className="w-full" style={{ border: '2px solid #8A1F21' }}>
+    <div className='w-full' style={{ border: '2px solid #8A1F21', borderRadius: '15px' }}>
       <ProgressBar
         completed={num}
-        bgColor="#8A1F21"
-        labelColor="#8A1F21"
-        baseBgColor="#FFF"
-        borderRadius="0px"
+        bgColor='#8A1F21'
+        labelColor='#8A1F21'
+        baseBgColor='#FFF'
+        borderRadius='15px'
       />
     </div>
   );
