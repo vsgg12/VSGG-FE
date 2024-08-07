7a5a5caa6bf3c1552d527bde4322763cad586314
정재로 <91364411+jjaeho0415@users.noreply.github.com>
2024-08-07 16:18:52 +0900

    fix: 다시 수정..

--- src/_lib/refreshTokenExpired.ts
+++ src/_lib/refreshTokenExpired.ts
@@ -10,9 +10,9 @@ const RefreshTokenExpired = () => {
     console.log('refreshTokenExpired 함수 시작점 도달 성공');
     useAuthStore.setState({ isLogin: false, accessToken: '', refreshToken: '' });
     localStorage.clear();
-    console.log("로컬은 초기화 완료했고 로그인 페이지로 이동하기 전 콘솔")
+    console.log('로컬은 초기화 완료했고 로그인 페이지로 이동하기 전 콘솔');
     router.replace('/login');
-  }, [router]);
+  }, []);
 
   return null;
 };
