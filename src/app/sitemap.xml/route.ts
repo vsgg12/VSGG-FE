0c2ffc917425eb2630fa683366c781d22c40d66a
정재로 <91364411+jjaeho0415@users.noreply.github.com>
2024-08-26 17:29:56 +0900

    chore: sitemap 수정 완료

--- src/app/sitemap.xml/route.ts
+++ src/app/sitemap.xml/route.ts
@@ -1,10 +1,10 @@
 import { NextResponse } from 'next/server';
 
-function generateSiteMap(posts: IGetPostDTOType[]) {
+function generateSiteMap(posts: IGetPostDTOType[], baseUrl: string) {
   return `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       <url>
-        <loc>http://localhost:3000</loc>
+        <loc>${baseUrl}</loc>
         <lastmod>${new Date().toISOString()}</lastmod>
         <priority>1.0</priority>
       </url>
@@ -12,7 +12,7 @@ function generateSiteMap(posts: IGetPostDTOType[]) {
         .map((post) => {
           return `
             <url>
-              <loc>http://localhost:3000/post/${post.id}</loc>
+              <loc>${baseUrl}/post/${post.id}</loc>
               <lastmod>${new Date(post.updatedAt).toISOString()}</lastmod>
               <priority>0.8</priority>
             </url>
@@ -24,14 +24,17 @@ function generateSiteMap(posts: IGetPostDTOType[]) {
 
 export async function GET() {
   try {
+    const baseUrl =
+      process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://vsgg.co.kr';
     // API 호출로 데이터를 가져옵니다.
-    const request = await fetch(`${process.env.NEXT_PUBLIC_PROXY_URL}`);
+    const request = await fetch(`${process.env.NEXT_PUBLIC_PROXY_URL}/post?orderby=&keyword=`);
     if (!request.ok) {
       throw new Error(`Failed to fetch posts: ${request.statusText}`);
     }
-    const posts: IGetPostDTOType[] = await request.json();
+    console.log(3);
+    const posts: IGetPostListType = await request.json();
     // 사이트맵 XML을 생성합니다.
-    const sitemap = generateSiteMap(posts);
+    const sitemap = generateSiteMap(posts.postDTO, baseUrl);
     // XML을 반환합니다.
     return new NextResponse(sitemap, {
       headers: {
