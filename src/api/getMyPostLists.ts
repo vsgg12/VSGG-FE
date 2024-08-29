6e51d414ea17c52e15102775ae10c3868391f3d3
정재로 <91364411+jjaeho0415@users.noreply.github.com>
2024-08-29 22:23:19 +0900

    feat: 내가 작성한 게시물 목록 받아오는 api 연동 확인 작업

--- src/api/getMyPostLists.ts
+++ src/api/getMyPostLists.ts
@@ -2,7 +2,7 @@ import api from "@/_lib/fetcher";
 
 export default async function getMyPostLists(token: string) {
     const data = await api.get<IGetMyPostsType>({
-        endpoint: "/users.post", authorization: token
+        endpoint: "/users/post", authorization: token
     });
     return data;
 }
