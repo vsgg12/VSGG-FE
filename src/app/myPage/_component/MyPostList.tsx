de633c8ddf6922c21bb698afa4c0d9ad2c275ed4
정재호 <91364411+jjaeho0415@users.noreply.github.com>
2024-09-28 17:47:11 +0900

    chore: 내가 쓴글 목록 컴포넌트화 작업

---
+++ src/app/myPage/_component/MyPostList.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import MyPostItem from './MyPostItem';
+
+interface IMyPostListProps{
+    myPostList: IGetMyPostItemsType[];
+}
+
+function MyPostList({myPostList}: IMyPostListProps) {
+  return (
+    <div className="flex flex-col gap-[10px]">
+      {myPostList.map((myPostItem: IGetMyPostItemsType) => (
+        <MyPostItem myPostItem={myPostItem}/>
+      ))}
+    </div>
+  );
+}
+
+export default MyPostList
