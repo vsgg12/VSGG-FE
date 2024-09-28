d29d2af563104a25fe6991369170bd912f8594ad
정재호 <91364411+jjaeho0415@users.noreply.github.com>
2024-09-28 19:58:39 +0900

    feat: 내 판결 전적 조회 api 연동 예정에 따른 스켈레톤 로직 작성 및 내 판결 전적 목록과 아이템 컴포넌트화 작업

---
+++ src/app/myPage/_component/MyJudgeItem.tsx
@@ -0,0 +1,44 @@
+import { useRouter } from 'next/navigation';
+import React from 'react';
+import VoteStatusCircle from './VoteStatusCircle';
+import formatDate from '@/utils/formatDate';
+
+interface IMyJudgeItemProps {
+  myJudgeItem: IVotedPostItem;
+}
+
+function MyJudgeItem({ myJudgeItem }: IMyJudgeItemProps) {
+  const router = useRouter();
+
+  return (
+    <>
+      <div
+        className='flex justify-between items-center text-xs text-[#C3C3C3] cursor-pointer'
+        key={myJudgeItem.id}
+        onClick={() => {
+          router.push(`/post/${myJudgeItem.id}/`);
+        }}
+      >
+        <div className='flex gap-[10px] items-center'>
+          <div className='text-[#555555] font-medium text-[16px]'>{myJudgeItem.title}</div>
+          <VoteStatusCircle
+            text={
+              myJudgeItem.myVoteResult === null ? myJudgeItem.voteStatus : myJudgeItem.myVoteResult
+            }
+          />
+        </div>
+
+        <div className='flex justify-between w-[235px]'>
+          <div className="flex gap-[5px]">
+            {myJudgeItem.authorNickname}{' '}
+            <span className='text-[#C3C3C3]'>{myJudgeItem.authorTier}</span>
+          </div>
+          <div>{formatDate(myJudgeItem.createdDate)}</div>
+        </div>
+      </div>
+      <div className='h-0.5 w-full bg-[#8A1F21] my-[8px]' />
+    </>
+  );
+}
+
+export default MyJudgeItem;
