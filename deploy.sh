327f78cf7cbaeaf4849f7dc8ec8c7656012ac0f8
jodonghyeon3 <jawoo1003@gmail.com>
2024-11-13 00:36:59 +0900

    Feat: CI/CD 구성을 위한 scripts 파일 구성

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 
 REPOSITORY=/home/ubuntu/vsgg
 REPOSITORY_PROD=/home/ubuntu/vsgg
@@ -7,7 +7,19 @@ echo "DEPLOYMENT_GROUP_NAME: ${DEPLOYMENT_GROUP_NAME}"
 
 echo "개발 서버 배포"
 cd "${REPOSITORY}"
+
+# NVM 환경 설정
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+
+# 의존성 설치
 npm install
+
+# 빌드 실행
+npm run build
+
+# PM2 프로세스 확인 및 시작/재시작
 pm2 describe vsgg > /dev/null
 if [ $? -eq 0 ]; then
     # 실행 중인 경우
