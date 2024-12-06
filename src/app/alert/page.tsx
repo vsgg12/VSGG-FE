ce92d5ffbb8d8f38422dbab42c86c5c54ea824da
정재호 <91364411+jjaeho0415@users.noreply.github.com>
2024-12-06 17:40:58 +0900

    feat: 모바일 버전의 알림 페이지 처음 렌더링 될때 isMobileVersion 여부 검사해서 false면 pc버전이므로 /home으로 replace되도록 함

--- src/app/alert/page.tsx
+++ src/app/alert/page.tsx
@@ -7,6 +7,8 @@ import { useAuthStore } from '../login/store/useAuthStore';
 import MobileHeader from '@/components/mobile/MobileHeader';
 import AlarmList from '../home/_component/AlarmList';
 import Loading from '@/components/Loading';
+import { useMobileVersionStore } from '@/store/useMobileVersionStore';
+import { useRouter } from 'next/navigation';
 
 const alarmTypes = [
   { type: '전체', value: 'ALL' },
@@ -22,6 +24,14 @@ function Alert() {
     queryFn: () => getAlarms(accessToken),
     enabled: isLogin,
   });
+  const [isPageLoading, setIsPageLoading] = useState<boolean>(true);
+  const { isMobileVersion } = useMobileVersionStore();
+  const router = useRouter();
+
+  useEffect(() => {
+    !isMobileVersion && router.replace('/home') 
+    setIsPageLoading(false);
+  },[])
 
   const filteredAlarms =
     alarmsData?.alarmList && alarmType !== '전체'
@@ -32,44 +42,48 @@ function Alert() {
 
   return (
     <div className='pb-[20px]'>
-      <MobileHeader headerTitle='알림' />
-      <div className='mobile-layout h-[100dvh] flex flex-col items-center px-[20px] py-[20px]'>
-        <div className='w-full h-[32px] border-b-1 border-[#ECECEC] flex mb-[20px]'>
-          {alarmTypes.map((alarm, idx) => (
-            <div
-              className={`h-full flex items-center justify-center cursor-pointer text-[18px] font-semibold ${alarm.type === alarmType ? 'text-black border-b-2 border-black rounded-sm' : 'text-[#AAAAAA]'}`}
-              style={{ width: 'calc(100% / 3)' }}
-              key={idx}
-              onClick={() => setAlarmType(alarm.type)}
-            >
-              {alarm.type}
+      {isPageLoading ? null : (
+        <>
+          <MobileHeader headerTitle='알림' />
+          <div className='mobile-layout h-[100dvh] flex flex-col items-center px-[20px] py-[20px]'>
+            <div className='w-full h-[32px] border-b-1 border-[#ECECEC] flex mb-[20px]'>
+              {alarmTypes.map((alarm, idx) => (
+                <div
+                  className={`h-full flex items-center justify-center cursor-pointer text-[18px] font-semibold ${alarm.type === alarmType ? 'text-black border-b-2 border-black rounded-sm' : 'text-[#AAAAAA]'}`}
+                  style={{ width: 'calc(100% / 3)' }}
+                  key={idx}
+                  onClick={() => setAlarmType(alarm.type)}
+                >
+                  {alarm.type}
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
-        {isLoading ? (
-          <div className='flex flex-grow h-full justify-center items-center'>
-            <Loading />
+            {isLoading ? (
+              <div className='flex flex-grow h-full justify-center items-center'>
+                <Loading />
+              </div>
+            ) : (
+              alarmsData && <AlarmList alarms={filteredAlarms} />
+            )}
+            {filteredAlarms && (
+              <div className='relative mt-[20px]'>
+                <div className='border-b-1 border-[#8A1F21] w-[301px]' />
+                <span
+                  className='text-[#828282] font-medium text-[10px] bg-[#F3F3F3] px-2 whitespace-nowrap'
+                  style={{
+                    position: 'absolute',
+                    bottom: '-8px',
+                    left: '50%',
+                    transform: 'translateX(-50%)',
+                  }}
+                >
+                  최대 14일 전까지의 알림을 확인할 수 있어요
+                </span>
+              </div>
+            )}
           </div>
-        ) : (
-          alarmsData && <AlarmList alarms={filteredAlarms} />
-        )}
-        {filteredAlarms && (
-          <div className='relative mt-[20px]'>
-            <div className='border-b-1 border-[#8A1F21] w-[301px]' />
-            <span
-              className='text-[#828282] font-medium text-[10px] bg-[#F3F3F3] px-2 whitespace-nowrap'
-              style={{
-                position: 'absolute',
-                bottom: '-8px',
-                left: '50%',
-                transform: 'translateX(-50%)',
-              }}
-            >
-              최대 14일 전까지의 알림을 확인할 수 있어요
-            </span>
-          </div>
-        )}
-      </div>
+        </>
+      )}
     </div>
   );
 }
