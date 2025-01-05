f8af35eddf391052a5995fbb4cee8f56950ebd9d
정재호 <91364411+jjaeho0415@users.noreply.github.com>
2025-01-06 00:04:40 +0900

    style: 모바일 내가쓴글, 판결전적, 프로필변경 페이지의 기본 뼈대 퍼블리싱(헤더)

--- src/app/myPage/mobile/modifyProfile/page.tsx
+++ src/app/myPage/mobile/modifyProfile/page.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import Loading from '@/components/Loading';
+import MobileHeader from '@/components/mobile/MobileHeader';
+import { useMobileVersionStore } from '@/store/useMobileVersionStore';
+import { useRouter } from 'next/navigation';
+import React, { useEffect, useState } from 'react'
 
 function ModifyProfile_Mobile() {
-  return (
-    <div>ModifyProfile_Mobile</div>
-  )
+   const [isPageLoading, setIsPageLoading] = useState<boolean>(true);
+
+   const { isMobileVersion } = useMobileVersionStore.getState();
+   // const { accessToken } = useAuthStore.getState();
+
+   const router = useRouter();
+
+   // const { data: myPostLists } = useQuery({
+   //   queryKey: ['MY_POST_LISTS', page],
+   //   queryFn: () => getMyPostLists({ token: accessToken, size: '10', page: String(page) }),
+   // });
+
+   useEffect(() => {
+     if (isMobileVersion === false) {
+       router.replace('/myPage/judgeRecord');
+     }
+     setIsPageLoading(false);
+   }, []);
+
+   return (
+     <div className='px-[10px]'>
+       {isPageLoading ? (
+         <div className='w-full h-[100dvh] items-center flex'>
+           <Loading />
+         </div>
+       ) : (
+         <>
+           <MobileHeader headerTitle='프로필 변경' />
+           <div className='mobile-layout h-[100dvh] flex flex-col items-center px-[21px] py-[20px] gap-[70px]'></div>
+         </>
+       )}
+     </div>
+   );
 }
 
 export default ModifyProfile_Mobile
