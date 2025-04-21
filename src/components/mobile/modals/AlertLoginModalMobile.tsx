2f3b865e9362057ab70c6480eec114a56520726f
정재호 <91364411+jjaeho0415@users.noreply.github.com>
2025-04-21 17:27:09 +0900

    style: 모바일/PC 버전 로그인 모달에도 구글, 카카오 로그인 버튼 추가 및 연동 로직 작성

--- src/components/mobile/modals/AlertLoginModalMobile.tsx
+++ src/components/mobile/modals/AlertLoginModalMobile.tsx
@@ -1,25 +1,51 @@
-import getNaverURL from '@/api/naver/getNaverURL';
+import getGoogleURL from '@/api/login/getGoogleUrl';
+import getKakaoURL from '@/api/login/getKakaoUrl';
+import getNaverURL from '@/api/login/getNaverURL';
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
-import { SiNaver } from 'react-icons/si';
+import { SiGoogle, SiNaver } from 'react-icons/si';
+import kakaoIcon from '../../../../public/svg/login/kakaoIcon.svg';
+import Image from 'next/image';
 
 function AlertLoginModal_Mobile() {
   const { data: NAVER_AUTH_URL, isLoading } = useQuery({
     queryKey: ['NAVER_URL'],
     queryFn: async () => getNaverURL(),
   });
+  const { data: GOOGLE_AUTH_URL } = useQuery({
+    queryKey: ['GOOGLE_URL'],
+    queryFn: () => getGoogleURL(),
+  });
+
+  const { data: KAKAO_AUTH_URL } = useQuery({
+    queryKey: ['KAKAO_URL'],
+    queryFn: () => getKakaoURL(),
+  });
 
   const NaverLogin = () => {
     if (NAVER_AUTH_URL) {
-      window.location.href = NAVER_AUTH_URL.naverLoginUrl;
+      window.location.href = NAVER_AUTH_URL.loginUrl;
     }
   };
+
+  const GoogleLogin = () => {
+    if (GOOGLE_AUTH_URL) {
+      window.location.href = GOOGLE_AUTH_URL.loginUrl;
+    }
+  };
+
+  const KakaoLogin = () => {
+    if (KAKAO_AUTH_URL) {
+      window.location.href = KAKAO_AUTH_URL.loginUrl;
+    }
+  };
+
   return (
     <div>
       {!isLoading && (
         <>
           <div
-            className='flex flex-col relative bg-white w-[305px] h-[399px] rounded-[30px] py-[80px] gap-[50px] '
+            className='flex flex-col relative bg-white w-[305px] min-h-[399px] rounded-[30px] py-[80px] gap-[50px] '
             onClick={(e) => e.stopPropagation()}
           >
             <div className='flex flex-col gap-[30px] justify-center items-center'>
@@ -34,12 +60,30 @@ function AlertLoginModal_Mobile() {
                   <p>자유롭게 이용해보세요!</p>
                 </div>
               </div>
-              <div onClick={NaverLogin} className='cursor-pointer'>
-                <div className='flex items-center justify-center gap-[5px] rounded-[30px] bg-black p-2  w-[222px] h-[50px]'>
-                  <SiNaver color='white' />
-                  <button className='text-white whitespace-nowrap text-[20px] font-extrabold'>
-                    네이버로 시작하기
-                  </button>
+              <div className='flex flex-col gap-[15px]'>
+                <div onClick={GoogleLogin} className='cursor-pointer'>
+                  <div className='flex items-center justify-center gap-[5px] rounded-[30px] bg-black p-2 w-[222px] h-[50px]'>
+                    <SiGoogle color='white' />
+                    <button className='text-white whitespace-nowrap text-[20px] font-extrabold'>
+                      구글로 시작
+                    </button>
+                  </div>
+                </div>
+                <div onClick={NaverLogin} className='cursor-pointer'>
+                  <div className='flex items-center justify-center gap-[5px] rounded-[30px] bg-black p-2 w-[222px] h-[50px]'>
+                    <SiNaver color='white' />
+                    <button className='text-white whitespace-nowrap text-[20px] font-extrabold'>
+                      네이버로 시작
+                    </button>
+                  </div>
+                </div>
+                <div onClick={KakaoLogin} className='cursor-pointer'>
+                  <div className='flex items-center justify-center gap-[5px] rounded-[30px] bg-black p-2 w-[222px] h-[50px]'>
+                    <Image src={kakaoIcon} width={17} height={16} alt='카카오아이콘' />
+                    <button className='text-white whitespace-nowrap text-[20px] font-extrabold'>
+                      카카오톡으로 시작
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
--- src/components/modals/AlertLoginModal.tsx
+++ src/components/modals/AlertLoginModal.tsx
@@ -1,12 +1,25 @@
+import getGoogleURL from '@/api/login/getGoogleUrl';
+import getKakaoURL from '@/api/login/getKakaoUrl';
 import getNaverURL from '@/api/login/getNaverURL';
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
-import { SiNaver } from 'react-icons/si';
+import { SiGoogle, SiNaver } from 'react-icons/si';
+import kakaoIcon from '../../../public/svg/login/kakaoIcon.svg';
+import Image from 'next/image';
 
 function AlertLoginModal() {
   const { data: NAVER_AUTH_URL, isLoading } = useQuery({
     queryKey: ['NAVER_URL'],
     queryFn: async () => getNaverURL(),
+  });
+  const { data: GOOGLE_AUTH_URL } = useQuery({
+    queryKey: ['GOOGLE_URL'],
+    queryFn: () => getGoogleURL(),
+  });
+
+  const { data: KAKAO_AUTH_URL } = useQuery({
+    queryKey: ['KAKAO_URL'],
+    queryFn: () => getKakaoURL(),
   });
 
   const NaverLogin = () => {
@@ -14,12 +27,25 @@ function AlertLoginModal() {
       window.location.href = NAVER_AUTH_URL.loginUrl;
     }
   };
+
+  const GoogleLogin = () => {
+    if (GOOGLE_AUTH_URL) {
+      window.location.href = GOOGLE_AUTH_URL.loginUrl;
+    }
+  };
+
+  const KakaoLogin = () => {
+    if (KAKAO_AUTH_URL) {
+      window.location.href = KAKAO_AUTH_URL.loginUrl;
+    }
+  };
+
   return (
     <div>
       {!isLoading && (
         <>
           <div
-            className='flex flex-col relative bg-white w-[674px] h-[310px] rounded-[18px] py-[80px] gap-[50px] px-[100px]'
+            className='flex flex-col relative bg-white w-[674px] min-h-[310px] rounded-[18px] py-[80px] gap-[50px] px-[100px]'
             onClick={(e) => e.stopPropagation()}
           >
             <div className='flex flex-col gap-[40px] justify-center items-center'>
@@ -31,12 +57,24 @@ function AlertLoginModal() {
                   간편하게 로그인하고 모든 기능을 자유롭게 이용해보세요!
                 </p>
               </div>
-              <div onClick={NaverLogin} className='cursor-pointer'>
-                <div className='mb-3 flex items-center justify-center gap-2 rounded-3xl bg-black p-2 px-32 '>
-                  <SiNaver color='white' />
-                  <button className='text-white whitespace-nowrap'>
-                    네이버로 3초만에 시작하기
-                  </button>
+              <div className='flex flex-col gap-[15px]'>
+                <div onClick={GoogleLogin} className='cursor-pointer'>
+                  <div className='flex items-center justify-center gap-2 rounded-3xl bg-black p-2 px-32 '>
+                    <SiGoogle color='white' />
+                    <button className='text-white whitespace-nowrap'>구글로 시작</button>
+                  </div>
+                </div>
+                <div onClick={NaverLogin} className='cursor-pointer'>
+                  <div className='flex items-center justify-center gap-2 rounded-3xl bg-black p-2 px-32 '>
+                    <SiNaver color='white' />
+                    <button className='text-white whitespace-nowrap'>네이버로 시작</button>
+                  </div>
+                </div>
+                <div onClick={KakaoLogin} className='cursor-pointer'>
+                  <div className='flex items-center justify-center gap-2 rounded-3xl bg-black p-2 px-32 '>
+                    <Image src={kakaoIcon} width={17} height={16} alt='카카오아이콘' />
+                    <button className='text-white whitespace-nowrap'>카카오톡으로 시작</button>
+                  </div>
                 </div>
               </div>
             </div>
