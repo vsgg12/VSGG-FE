5bbcf37654038de7cb0909d7c9d5243d226998f5
정재호 <91364411+jjaeho0415@users.noreply.github.com>
2025-05-01 22:34:23 +0900

    style: swiper을 이용한 모바일/PC버전 배너 퍼블리싱

---
+++ src/app/home/mobile/component/VerticalBannerSwiper.tsx
@@ -0,0 +1,60 @@
+'use client';
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Autoplay, Mousewheel, Navigation } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { useRouter } from 'next/navigation';
+
+function VerticalBannerSwiper() {
+  const router = useRouter();
+
+  const handleFeedbackBannerClick = () => {
+    router.push('https://forms.gle/RN9FPwW6UgrExoTo8');
+  };
+  const handleUploadBannerClick = () => {
+    router.push('https://forms.gle/gFMtaNWqMZN7oqJW7');
+  };
+
+  return (
+    <div className='w-full flex gap-[10px]'>
+      <Swiper
+        direction='vertical'
+        mousewheel={true}
+        modules={[Autoplay, Pagination, Mousewheel, Navigation]}
+        spaceBetween={30}
+        pagination={{
+          clickable: false,
+          el: '.custom-pagination',
+        }}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+        loop={true}
+        className='w-[150.8px] h-[59.47px]'
+      >
+        <SwiperSlide>
+          <img
+            src='/images/banner/VSGG배너_피드백시RP증정.png'
+            alt='피드백배너'
+            className='w-full h-full cursor-pointer'
+            onClick={handleFeedbackBannerClick}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src='/images/banner/VSGG배너_영상업로드시RP증정.png'
+            alt='영상업로드배너'
+            className='w-full h-full cursor-pointer'
+            onClick={handleUploadBannerClick}
+          />
+        </SwiperSlide>
+      </Swiper>
+      <div className='custom-pagination h-full flex self-end rotate-90' />
+    </div>
+  );
+}
+
+export default VerticalBannerSwiper;
