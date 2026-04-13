64bfe9552a48384ebd89ddd96061d7d23dcd3889
정재호 <jjh0773@naver.com>
2026-04-14 00:04:08 +0900

    chore: 빌드 오류 수정

--- src/components/modals/MoreModal.tsx
+++ src/components/modals/MoreModal.tsx
@@ -66,26 +66,23 @@ function MoreModal({
   const handleClick = (text: string) => {
     switch (text) {
       case '수정':
-        // where에 따른 댓글 수정, 게시글 수정 api 호출 다르게
-        alert('준비중입니다.');
+        if (where === 'post') {
+          if (!isEditPostPossible) {
+            return alert('게시글은 판결기간 종료 24시간 전까지만 수정이 가능합니다.');
+          }
+          alert('준비중입니다.');
+          // router.push(`/post/${postId}/edit`);
+        }
         break;
-      // if (where === 'post') {
-      //   if (!isEditPostPossible) {
-      //     return alert('게시글은 판결기간 종료 24시간 전까지만 수정이 가능합니다.');
-      //   }
-      //   router.push(`/post/${postId}/edit`);
-      // }
-      // break;
       case '삭제':
-        alert('준비중입니다.');
-        // if (
-        //   where === 'post' &&
-        //   confirm('이 게시글을 삭제하면 복구할 수 없습니다.\n게시글을 삭제하시겠습니까?')
-        // ) {
-        //   deletePostItem();
-        // } else if (where === 'comment' && confirm('댓글을 삭제하시겠습니까?')) {
-        //   deleteComment();
-        // }
+        if (
+          where === 'post' &&
+          confirm('이 게시글을 삭제하면 복구할 수 없습니다.\n게시글을 삭제하시겠습니까?')
+        ) {
+          deletePostItem();
+        } else if (where === 'comment' && confirm('댓글을 삭제하시겠습니까?')) {
+          deleteComment();
+        }
         break;
       case '신고':
         // 이건 어떻게 할지 아직 모름
