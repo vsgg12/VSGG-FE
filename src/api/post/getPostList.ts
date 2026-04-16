5070c95175ba9a43fddc2ca71f363a983a3b01d3
정재호 <jjh0773@naver.com>
2026-04-16 21:58:18 +0900

    fix: 홈 화면 게시글 전체 목록 조회 무한 스크롤로 api 페이지네이션 수정에 따른 로직 수정완료

--- src/api/post/getPostList.ts
+++ src/api/post/getPostList.ts
@@ -1,10 +1,13 @@
 import api from '@/_lib/fetcher';
 
-export default async function getPostList(orderBy: string, keyword: string, token: string) {
-  const data = await api.get<IGetPostListType>({
-    endpoint: `/post?orderby=${orderBy}&keyword=${keyword}`,
+export default async function getPostList(
+  orderBy: string,
+  keyword: string,
+  token: string,
+  page: number,
+): Promise<IGetPostListType> {
+  return await api.get<IGetPostListType>({
+    endpoint: `/post?orderby=${orderBy}&keyword=${keyword}&page=${page}`,
     authorization: token,
   });
-
-  return data;
 }
