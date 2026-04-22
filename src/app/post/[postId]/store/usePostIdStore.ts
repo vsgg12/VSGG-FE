ac8df283113d03a1d25900257be47d2636b1d9a9
shuding0307 <suhyun2116@gmail.com>
2026-04-22 19:12:36 +0900

    fix/ votingGraph 토글시 리셋되는 오류 수정

--- src/app/post/[postId]/store/usePostIdStore.ts
+++ src/app/post/[postId]/store/usePostIdStore.ts
@@ -16,7 +16,9 @@ interface PostIdStore {
 
 const initialPostVoteState = {
   voteResult: [],
+  setVoteResult: (vote: number[]) => set({ voteResult: vote }),
   votingGraph: Array(10).fill(-1),
+  setVotingGraph: (graph: number[]) => set({ votingGraph: graph }),
   postVoteResult: [],
   selectedChampIdx: 0,
   isNotAbleSubmit: true,
