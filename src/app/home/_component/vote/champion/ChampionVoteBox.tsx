89805348b11aa939611030ed5e6bd224c2cb4a93
shuding0307 <suhyun2116@gmail.com>
2026-04-27 10:34:38 +0900

    fix/textSize

--- src/app/home/_component/vote/champion/ChampionVoteBox.tsx
+++ src/app/home/_component/vote/champion/ChampionVoteBox.tsx
@@ -70,7 +70,7 @@ const ChampionVoteBox = ({
           >
             {sortedVoteData.map((item, idx) => (
               <div key={item.inGameInfoId} onMouseEnter={() => setIsHover(idx)}>
-                <ChampionVoteResultItem voteItem={item} isHover={isHover === idx} />
+                <ChampionVoteResultItem voteItem={item} isHover={isHover === idx} isHome={true} />
               </div>
             ))}
           </div>
@@ -87,23 +87,22 @@ const ChampionVoteBox = ({
       {/* 안내 문구 및 버튼 */}
       {shouldBlur && (
         <div className='absolute inset-0 flex flex-col justify-center items-center z-50 text-white gap-4'>
-          {!isLogin ||
-            (isHome && (
-              <>
-                <div
-                  className={`flex flex-col items-center gap-1 drop-shadow-lg ${isHome ? 'text-[12px]' : 'text-[20px]'} font-bold`}
-                >
-                  <p>판결이 궁금하시다구요?</p>
-                  <p>판결에 참여하고, 결과를 확인하세요</p>
-                </div>
-                <button
-                  onClick={() => setIsLoginModalOpen(true)}
-                  className={`bg-[#8A1F21] hover:bg-[#a02426] text-white w-[173px] h-[41px] rounded-[5px] font-extrabold ${isHome ? 'text-[16px]' : 'text-[18px]'} transition-colors shadow-xl cursor-pointer`}
-                >
-                  지금 바로 판결하기
-                </button>
-              </>
-            ))}
+          {(!isLogin || isHome) && (
+            <>
+              <div
+                className={`flex flex-col items-center gap-1 drop-shadow-lg ${isHome ? 'text-[12px]' : 'text-[20px]'} font-bold`}
+              >
+                <p>판결이 궁금하시다구요?</p>
+                <p>판결에 참여하고, 결과를 확인하세요</p>
+              </div>
+              <button
+                onClick={() => setIsLoginModalOpen(true)}
+                className={`bg-[#8A1F21] hover:bg-[#a02426] text-white w-[173px] h-[41px] rounded-[5px] font-extrabold ${isHome ? 'text-[16px]' : 'text-[18px]'} transition-colors shadow-xl cursor-pointer`}
+              >
+                지금 바로 판결하기
+              </button>
+            </>
+          )}
 
           {/* 로그인 상태지만 투표 없음 */}
           {isLogin && isNoVote && !isVoteEnd && !isHome && (
