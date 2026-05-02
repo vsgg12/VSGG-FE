1ec5eb9410a096e2f16f5201c71e5e582c51df0d
shuding0307 <suhyun2116@gmail.com>
2026-05-02 21:17:10 +0900

    style/게시글 상세페이지 티어 추가

--- src/app/post/[postId]/desktop/components/Comment/CommentItem.tsx
+++ src/app/post/[postId]/desktop/components/Comment/CommentItem.tsx
@@ -1,4 +1,6 @@
+import useProfileTierIcon from '@/hooks/sidebar/useProfileTierIcon';
 import useTimeDifferenceFromNow from '@/hooks/useTimeDifferenceFromNow';
+import { truncateText } from '@/utils/truncateText';
 
 interface ICommentProps {
   comment: IGetCommentItemType;
@@ -10,14 +12,18 @@ interface ICommentProps {
 }
 
 export default function CommentItem({ comment, targetComment, handleReply }: ICommentProps) {
+  const { getIcon } = useProfileTierIcon({ size: 14 });
   const timeAgo = useTimeDifferenceFromNow(comment.createdDateTime);
 
   return (
     <div>
       <div className='flex flex-row relative font-medium items-center mb-[5px]'>
-        <p className='mr-[5px] text-[14px] text-[#333333] whitespace-nowrap font-semibold'>
-          {comment.member.nickname}
-        </p>
+        <div className='flex gap-[4px]'>
+          {getIcon(comment.member.tier)}
+          <p className='mr-[5px] text-[14px] text-[#333333] font-semibold'>
+            {truncateText(comment.member.nickname, 8)}
+          </p>
+        </div>
         <p className='text-[14px] text-[#909090] min-w-fit'>{comment.member.tier}</p>
         <p className='text-[12px] text-[#C8C8C8] ml-2 min-w-fit'>| {timeAgo}</p>
       </div>
