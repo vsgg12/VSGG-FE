e02234c8e44c21bd324bb02cdd253a6e755120bd
shuding0307 <suhyun2116@gmail.com>
2026-05-10 16:37:10 +0900

    fix: 문구 롤백

--- src/app/home/desktop/WritePost.tsx
+++ src/app/home/desktop/WritePost.tsx
@@ -16,8 +16,7 @@ function WritePost({ handleWriteClick, isListed }: Props) {
             우리 팀 플레이의 과실이 궁금하다면?
           </p>
           <p className='text-[14px] text-[#C8C8C8] whitespace-pre-line'>
-            {/* {`바텀 다이브 왜 안 함? 한타 구도가 이게 맞아? 진짜 너무 억울함 \n 우리팀이 넘 밉다..........`} */}
-            {`팀원들의 플레이에 대한 피드백이 필요하다면? \n 글 작성하기 버튼을 눌러보세요! \n VSGG 화이팅`}
+            {`바텀 다이브 왜 안 함? 한타 구도가 이게 맞아? 진짜 너무 억울함 \n 우리팀이 넘 밉다..........`}
           </p>
         </div>
         <button
