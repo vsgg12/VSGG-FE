073eb1fd762ddfbfbb413e86e2ff9a2886aadc01
이태림 <94667907+rimtae@users.noreply.github.com>
2026-05-31 18:25:55 +0900

    style: 댓글 UI 및 푸터 스타일 수정

--- src/app/home/desktop/PostCommentItem.tsx
+++ src/app/home/desktop/PostCommentItem.tsx
@@ -21,12 +21,11 @@ export default function PostCommentItem({ comment, targetComment, handleReply }:
       <div className='flex flex-row relative font-medium items-center mb-[5px]'>
         <div className='flex gap-[4px]'>
           {getIcon(comment.member.tier)}
-          <p className='mr-[5px] text-[14px] text-[#333333] font-semibold'>
+          <p className='mr-[5px] text-[14px] text-[#242526] font-semibold'>
             {truncateText(comment.member.nickname, 8)}
           </p>
         </div>
-        <p className='text-[14px] text-[#909090] min-w-fit'>{comment.member.tier}</p>
-        <p className='text-[12px] text-[#C8C8C8] ml-2 min-w-fit'>| {timeAgo}</p>
+          <p className='text-[12px] text-[#D7D8D9] font-medium ml-2 min-w-fit'>{timeAgo}</p>
       </div>
       <p className='text-[14px] mb-[2px]'>
         {targetComment && <span className='text-[#E20A29]'>@{comment.parentMemberNickname} </span>}
