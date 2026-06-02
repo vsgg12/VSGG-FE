a15c57baac93d7735b1e045baa1cd89d5c46fbe8
이태림 <94667907+rimtae@users.noreply.github.com>
2026-06-03 02:23:09 +0900

    feat: 모바일 홈 헤더 로고 수정

--- src/components/mobile/Headers/MainHeader.tsx
+++ src/components/mobile/Headers/MainHeader.tsx
@@ -61,74 +61,73 @@ function MainHeader() {
   };
 
   return (
-    <div className='flex gap-[10px] h-[44px] px-[10px] items-center justify-between mobile-layout sticky top-0 z-[40]'>
-      {/* 라이트모드용 로고: 기본적으로 보이고, 다크모드(.dark)에서는 숨김 */}
-      <img
-        src='/logo/vertical/logo-vertical-red.svg'
-        alt='VS.GG'
-        className={`block w-auto h-[20px] dark:block`}
-      />
-
-      {/* 다크모드용 로고: 기본적으로 숨기고, 다크모드(.dark)에서만 보임 */}
-      <img
-        src='/logo/vertical/logo-vertical-white.svg'
-        alt='VS.GG'
-        className={`hidden w-auto h-[20px] dark:hidden`}
-      />
-      {isLogin ? (
-        <div className='flex'>
-          <button
-            className={`relative group/alarm hd-items cursor-pointer `}
-            onClick={handleAlarmBtnClick}
-          >
-            <IoMdNotificationsOutline />
-            <span
-              className={`text-[#E20A29] text-[12px] font-bold flex flex-col relative items-center justify-center  w-[20px] h-[12px] p-0 m-0  ${(noReadAlarms === undefined || noReadAlarms === 0) && 'invisible'}`}
-              style={{
-                position: 'absolute',
-                transform: 'translate(5.5px,-22px)',
-              }}
+    <div className='flex gap-[10px] h-[80px] px-[20px] items-center justify-between mobile-layout sticky top-0 z-[40]'>
+        {/* 라이트모드용 로고: 기본적으로 보이고, 다크모드(.dark)에서는 숨김 */}
+        <img
+          src='/logo/horizontal/logo-horizontal-red.svg'
+          alt='VS.GG'
+          className={`block w-auto h-[30px] dark:block mt-[2px]`}
+        />
+        {/* 다크모드용 로고: 기본적으로 숨기고, 다크모드(.dark)에서만 보임 */}
+        <img
+          src='/logo/horizontal/logo-horizontal-red.svg'
+          alt='VS.GG'
+          className={`hidden w-auto h-[30px] dark:hidden mt-[2px]`}
+        />
+        {isLogin ? (
+          <div className='flex'>
+            <button
+              className={`relative group/alarm hd-items cursor-pointer `}
+              onClick={handleAlarmBtnClick}
             >
-              {data && noReadAlarms > 99 ? '99+' : `${noReadAlarms}`}
+              <IoMdNotificationsOutline />
               <span
-                className={`text-[#E20A29]  text-[12px] font-bold flex flex-col items-center justify-center w-[20px] h-[12px] p-0 m-0 text-stroke ${(noReadAlarms === undefined || noReadAlarms === 0) && 'invisible'}`}
+                className={`text-[#E20A29] text-[12px] font-bold flex flex-col relative items-center justify-center  w-[20px] h-[12px] p-0 m-0  ${(noReadAlarms === undefined || noReadAlarms === 0) && 'invisible'}`}
                 style={{
                   position: 'absolute',
-                  left: '0',
-                  top: '0',
-                  zIndex: '-1',
+                  transform: 'translate(5.5px,-22px)',
                 }}
               >
                 {data && noReadAlarms > 99 ? '99+' : `${noReadAlarms}`}
+                <span
+                  className={`text-[#E20A29]  text-[12px] font-bold flex flex-col items-center justify-center w-[20px] h-[12px] p-0 m-0 text-stroke ${(noReadAlarms === undefined || noReadAlarms === 0) && 'invisible'}`}
+                  style={{
+                    position: 'absolute',
+                    left: '0',
+                    top: '0',
+                    zIndex: '-1',
+                  }}
+                >
+                  {data && noReadAlarms > 99 ? '99+' : `${noReadAlarms}`}
+                </span>
               </span>
-            </span>
-            <span className='absolute top-[45px] flex justify-center items-center h-[23px] text-[12px] font-medium bg-white text-[#828282] rounded-[5px] p-[4px] whitespace-nowrap invisible group-hover/alarm:visible'>
-              알림
-            </span>
-          </button>
+              <span className='absolute top-[45px] flex justify-center items-center h-[23px] text-[12px] font-medium bg-white text-[#828282] rounded-[5px] p-[4px] whitespace-nowrap invisible group-hover/alarm:visible'>
+                알림
+              </span>
+            </button>
+            <button
+              className={`relative group/profile hd-items flex items-center justify-center rounded-full `}
+              onClick={handleProfileBtnClick}
+            >
+              <img
+                src={userProfileData?.memberProfileDTO.profileUrl}
+                alt='profileImage'
+                className='h-[24px] w-[24px] rounded-full border-[#E20A29] border-[2px]'
+              />
+              <span className='absolute top-[40px] left-[-3px] flex justify-center items-center h-[23px] text-[12px] font-medium bg-white text-[#828282] rounded-[5px] p-[4px] whitespace-nowrap invisible group-hover/profile:visible'>
+                프로필
+              </span>
+            </button>
+          </div>
+        ) : (
           <button
-            className={`relative group/profile hd-items flex items-center justify-center rounded-full `}
-            onClick={handleProfileBtnClick}
+            className='rounded-[150px] border-2 border-[#E20A29] px-[30px] py-[4px] text-[#E20A29] text-[16px] self-center'
+            onClick={handleLoginBtnClick}
           >
-            <img
-              src={userProfileData?.memberProfileDTO.profileUrl}
-              alt='profileImage'
-              className='h-[24px] w-[24px] rounded-full border-[#E20A29] border-[2px]'
-            />
-            <span className='absolute top-[40px] left-[-3px] flex justify-center items-center h-[23px] text-[12px] font-medium bg-white text-[#828282] rounded-[5px] p-[4px] whitespace-nowrap invisible group-hover/profile:visible'>
-              프로필
-            </span>
+            로그인
           </button>
-        </div>
-      ) : (
-        <button
-          className='rounded-[150px] border-2 border-[#E20A29] px-[30px] py-[5px] text-[#E20A29]'
-          onClick={handleLoginBtnClick}
-        >
-          로그인
-        </button>
-      )}
-    </div>
+        )}
+    </div>  
   );
 }
 
