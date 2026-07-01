baeab6e12186cccdb791ea9f0f48d8c1d315a21e
shuding0307 <suhyun2116@gmail.com>
2026-07-01 21:22:00 +0900

    style/mobileListItem

--- src/app/home/mobile/component/ListedPostItemMobile.tsx
+++ src/app/home/mobile/component/ListedPostItemMobile.tsx
@@ -1,68 +1,83 @@
-import { formatNumberWithCommas } from '@/utils/formatNumberWithCommas';
-import moment from 'moment';
 import { useRouter } from 'next/navigation';
 import PostDeadLineMobile from './PostDeadLineMobile';
-import { useEffect, useState } from 'react';
+import HeartIcon from '../../../../../public/svg/mobile/postItem/heart.svg';
+import CommentIcon from '../../../../../public/svg/mobile/postItem/comment.svg';
+import VoteIcon from '../../../../../public/svg/mobile/postItem/vote.svg';
+import ViewIcon from '../../../../../public/svg/mobile/postItem/view.svg';
+import Image from 'next/image';
+import useProfileTierIcon from '@/hooks/sidebar/useProfileTierIcon';
+import useTimeDifferenceFromNow from '@/hooks/useTimeDifferenceFromNow';
+import useConvertHTML from '@/hooks/useConvertHTML';
 
 interface IListedItem {
   post: IGetPostDTOType;
 }
 
 export default function ListedPostItemMobile({ post }: IListedItem) {
-  const [formattedDate, setFormattedDate] = useState<string>();
-
-  useEffect(() => {
-    setFormattedDate(moment(post.createdAt).format('YYYY.MM.DD. HH:mm'));
-  }, [post]);
   const router = useRouter();
+  const { getIcon } = useProfileTierIcon({ size: 12 });
+  const timeAgo = useTimeDifferenceFromNow(post.createdAt);
+  const contentsArr = useConvertHTML(post.content);
+
+  const icons = [
+    {
+      icon: HeartIcon,
+      alt: 'heartIcon',
+      content: post.likeCount > 999 ? '999+' : post.likeCount,
+    },
+    {
+      icon: VoteIcon,
+      alt: 'voteIcon',
+      content: post.voteCount > 999 ? '999+' : post.voteCount,
+    },
+    {
+      icon: CommentIcon,
+      alt: 'commentIcon',
+      content: post.commentCount > 999 ? '999+' : post.commentCount,
+    },
+    {
+      icon: ViewIcon,
+      alt: 'viewIcon',
+      content: post.viewCount > 999 ? '999+' : post.viewCount,
+    },
+  ];
   return (
     <div
-      className='flex flex-col w-full  min-h-[135px] bg-white mb-[20px] rounded-[30px] p-[25px] gap-[20px] cursor-pointer'
+      className='flex flex-col w-full min-h-[135px] bg-white mb-[20px] rounded-[30px] p-[25px] gap-[8px] cursor-pointer'
       onClick={() => {
         router.push(`/post/${post.id}/`);
       }}
     >
-      {/* <div className='flex'>
-        <div className='flex flex-grow gap-[8px]'>
-          <p className='text-[12px]'>{post.memberDTO.nickname}</p>
-          <p className='text-[#909090] text-[12px]'>{post.memberDTO.tier}</p>
-        </div>
-        <p className='text-[#C8C8C8] text-[12px]'>{moment(post.createdAt).format('YYYY-MM-DD')}</p>
+      <div className='flex justify-between items-center'>
+        <p className='text-black text-[16px] font-bold'>{post.title}</p>
+        <PostDeadLineMobile deadLine={post.daysUntilEnd} />
       </div>
-      <div className='flex justify-center items-center gap-[5px]'>
-        <div className='text-black text-[25px]'>{post.title}</div>
-        <div className='text-[#C8C8C8] flex-grow'>
-          | 조회수 {formatNumberWithCommas(post.viewCount)}
-        </div>
-        <PostDeadLine deadLine={post.daysUntilEnd} />
-      </div> */}
-      <div className='flex w-full justify-between'>
-        <div className='flex gap-[12px]'>
+      <p className='text-[14px] font-medium whitespace-nowrap overflow-hidden truncate text-[#484B4D]'>
+        {contentsArr.pTags[0]}
+      </p>
+      <p className='text-[#AEB1B2] text-[12px]'>{timeAgo}</p>
+
+      <div className='flex w-full justify-between items-center'>
+        <div className='flex gap-[4px] items-center'>
           <img
-            className='h-[32px] w-[32px] rounded-full'
+            className='h-[20px] w-[20px] rounded-full'
             src={
               post.memberDTO.profileImage === null
                 ? 'https://ssl.pstatic.net/static/pwe/address/img_profile.png'
                 : post.memberDTO.profileImage
             }
           />
-          <div className='flex flex-col'>
-            <div className='text-[12px] text-[#333333] max-w-[170px]'>
-              {post.memberDTO.nickname}
-              <span className='ml-[8px] text-[12px] text-[#909090]'>{post.memberDTO.tier}</span>
-            </div>
-            <p className='text-[12px] text-[#C8C8C8]'>{formattedDate}</p>
-          </div>
+          {getIcon(post.memberDTO.tier)}
+          <p className='font-bold text-[12px]'>{post.memberDTO.nickname}</p>
         </div>
-        <PostDeadLineMobile deadLine={post.daysUntilEnd} />
-      </div>
-      <div>
-        <p className='text-black text-[15px]'>
-          {post.title}
-          <span className='text-[#C8C8C8] text-[12px] ml-[10px]'>
-            | 조회수 {formatNumberWithCommas(post.viewCount)}
-          </span>
-        </p>
+        <div className='flex gap-[10px]'>
+          {icons.map((item, index) => (
+            <div key={index} className='flex gap-[4px] items-center'>
+              <Image src={item.icon} width={16} height={16} alt={item.alt} />
+              <p className='text-[12px] text-[#D7D8D9]'>{item.content}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
