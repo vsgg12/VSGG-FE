5233bca92e14dd80e8bdaf368201a5ef93ae4c70
shuding0307 <suhyun2116@gmail.com>
2026-07-01 18:30:02 +0900

    style: 모바일 홈 컴포넌트에 좋아요 기능 및 아이콘 추가

--- src/app/post/[postId]/mobile/components/ContentAreaMobile.tsx
+++ src/app/post/[postId]/mobile/components/ContentAreaMobile.tsx
@@ -4,10 +4,17 @@ import React, { useEffect, useState } from 'react';
 import Icon_more from '../../../../../../public/svg/Icon_more.svg';
 import { formatNumberWithCommas } from '@/utils/formatNumberWithCommas';
 import { useAuthStore } from '@/app/login/store/useAuthStore';
-import moment from 'moment';
 import DOMPurify from 'dompurify';
 import usePostIdStore from '../../store/usePostIdStore';
 import PostDeadLineMobile from '@/app/home/mobile/component/PostDeadLineMobile';
+import useTimeDifferenceFromNow from '@/hooks/useTimeDifferenceFromNow';
+import Icon_eye from '../../../../../../public/svg/postItem/eye.svg';
+import Icon_heart from '../../../../../../public/svg/postItem/heart.svg';
+import Icon_heart_hover from '../../../../../../public/svg/postItem/heart_hover.svg';
+import postPostLike from '@/api/like/postPostLike';
+import patchCancelLike from '@/api/like/patchCancelLike';
+import { useMutation } from '@tanstack/react-query';
+import { useLoginStore } from '@/store/login/useLoginStore';
 
 interface IContentArea {
   isOwner: boolean;
@@ -17,15 +24,20 @@ interface IContentArea {
 const videoStyle = 'w-full rounded-[14px] block visible aspect-video bg-black object-contain';
 
 function ContentAreaMobile({ isOwner, post }: IContentArea) {
-  const { user } = useAuthStore();
+  const { user, accessToken, isLogin } = useAuthStore();
   const { voteResult, setPostVoteResult } = usePostIdStore();
-  const [formattedDate, setFormattedDate] = useState<string>('');
   const [sanitizedHtml, setSanitizedHtml] = useState<string>('');
   const [isMoreModalOpen, setIsMoreModalOpen] = useState<boolean>(false);
+  const timeAgo = useTimeDifferenceFromNow(post.postDTO.createdAt);
+  const [updatedLikeCount, setUpdatedLikeCount] = useState<number | null>(null);
+  const [isHovered, setIsHovered] = useState<string>('');
+  const [isLiked, setIsLiked] = useState<boolean>(false);
+  const [heartIcon, setHeartIcon] = useState<string>(Icon_heart);
+  const { setIsLoginModalOpen } = useLoginStore();
+  const [isLikeInProgress, setIsLikeInProgress] = useState<boolean>(false);
 
   useEffect(() => {
     if (post && user) {
-      setFormattedDate(moment(post.postDTO.createdAt).format('YYYY-MM-DD'));
       const sanitize = DOMPurify.sanitize(post.postDTO.content);
       setSanitizedHtml(sanitize);
 
@@ -38,6 +50,87 @@ function ContentAreaMobile({ isOwner, post }: IContentArea) {
       setPostVoteResult(newPostVoteResult);
     }
   }, [post, voteResult, setPostVoteResult, user]);
+
+  useEffect(() => {
+    setIsLiked(post.postDTO.liked);
+
+    if (post.postDTO.liked == true) {
+      setHeartIcon(Icon_heart_hover);
+    }
+  }, [post]);
+
+  useEffect(() => {
+    if (isLiked) {
+      setHeartIcon(Icon_heart_hover);
+    } else {
+      setHeartIcon(Icon_heart);
+    }
+  }, [isLiked]);
+
+  useEffect(() => {
+    if (!isLiked) {
+      if (isHovered == 'like') {
+        setHeartIcon(Icon_heart_hover);
+      } else {
+        setHeartIcon(Icon_heart);
+      }
+    }
+  }, [isHovered, isLiked]);
+
+  const { mutate: likePost } = useMutation({
+    mutationFn: async () => {
+      const response = await postPostLike(accessToken, post.postDTO.id);
+      return response.postLikeDTO.likeCount;
+    },
+    onMutate: () => {
+      setIsLikeInProgress(true);
+    },
+    onSuccess: (likeCount) => {
+      setIsLiked(true);
+      setIsLikeInProgress(false);
+      setUpdatedLikeCount(likeCount);
+    },
+    onError: (error) => {
+      console.error(error.message);
+      setIsLikeInProgress(false);
+    },
+  });
+
+  const { mutate: unlikePost } = useMutation({
+    mutationFn: async () => {
+      const response = await patchCancelLike(accessToken, post.postDTO.id);
+      return response.postLikeDTO.likeCount;
+    },
+    onMutate: () => {
+      setIsLikeInProgress(true);
+    },
+    onSuccess: (likeCount) => {
+      setIsLiked(false);
+      setIsLikeInProgress(false);
+      setUpdatedLikeCount(likeCount);
+    },
+    onError: (error) => {
+      console.error(error.message);
+      setIsLikeInProgress(false);
+    },
+  });
+
+  const handleLikePost = async (e: React.MouseEvent<HTMLImageElement>) => {
+    e.stopPropagation();
+    if (!isLogin) {
+      setIsLoginModalOpen(true);
+      return;
+    }
+    if (isLikeInProgress) {
+      return;
+    }
+
+    if (isLiked) {
+      await unlikePost();
+    } else {
+      await likePost();
+    }
+  };
 
   const handleMoreIconClick = () => {
     setIsMoreModalOpen(!isMoreModalOpen);
@@ -53,27 +146,33 @@ function ContentAreaMobile({ isOwner, post }: IContentArea) {
     <div className='h-fit w-full'>
       {post && (
         <div className='h-fit w-full rounded-[30px] bg-[#ffffff] flex flex-col mb-[35px] p-[30px] gap-[15px]'>
-          <div className='flex w-full justify-between'>
-            <div className='flex gap-[10px]'>
-              <img
-                className='h-[32px] w-[32px] rounded-full'
-                src={
-                  post.postDTO.memberDTO.profileImage === null
-                    ? 'https://ssl.pstatic.net/static/pwe/address/img_profile.png'
-                    : post.postDTO.memberDTO.profileImage
-                }
-              />
-              <div className='flex flex-col'>
-                <div className='text-[12px] text-[#333333] max-w-[170px]'>
-                  {post.postDTO.memberDTO.nickname}
-                  <span className='ml-[8px] text-[12px] text-[#909090]'>
-                    {post.postDTO.memberDTO.tier}
-                  </span>
+          <div className='flex w-full'>
+            <div className='flex gap-[10px] w-full justify-between items-center'>
+              <div className='flex gap-[10px]'>
+                <img
+                  className='h-[32px] w-[32px] rounded-full'
+                  src={
+                    post.postDTO.memberDTO.profileImage === null
+                      ? 'https://ssl.pstatic.net/static/pwe/address/img_profile.png'
+                      : post.postDTO.memberDTO.profileImage
+                  }
+                />
+                <div className='flex flex-col'>
+                  <div className='text-[12px] text-[#333333] max-w-[170px]'>
+                    {post.postDTO.memberDTO.nickname}
+                  </div>
+                  <div className='flex text-[12px] text-[#C8C8C8] items-center gap-[3px]'>
+                    <p>{timeAgo} ・ </p>
+                    <Image src={Icon_eye} width={16} height={16} alt='eyeIcon' />
+                    <p> {formatNumberWithCommas(post.postDTO.viewCount)}</p>
+                  </div>
                 </div>
-                <p className='text-[12px] text-[#C8C8C8]'>{formattedDate}</p>
               </div>
               <PostDeadLineMobile deadLine={post.postDTO.daysUntilEnd} />
             </div>
+          </div>
+          <div className='flex justify-between'>
+            <p className='text-[#242526] text-[20px] whitespace-nowrap'>{post.postDTO.title}</p>
             <div className='flex'>
               {isMoreModalOpen && (
                 <div className='absolute right-[75px] translate-y-[5px]'>
@@ -93,14 +192,6 @@ function ContentAreaMobile({ isOwner, post }: IContentArea) {
                 onClick={handleMoreIconClick}
               />
             </div>
-          </div>
-          <div className='flex'>
-            <p className='text-black text-[15px] whitespace-nowrap'>
-              {post.postDTO.title}
-              <span className='text-[#C8C8C8] text-[12px] ml-[10px]'>
-                | 조회수 {formatNumberWithCommas(post.postDTO.viewCount)}
-              </span>
-            </p>
           </div>
           <div className='flex flex-col w-full'>
             {post.postDTO.video.type === 'FILE' ? (
@@ -136,6 +227,19 @@ function ContentAreaMobile({ isOwner, post }: IContentArea) {
                 dangerouslySetInnerHTML={{ __html: sanitizedHtml }}
               ></div>
             </div>
+          </div>
+          <div
+            className='flex cursor-pointer items-center justify-center'
+            onClick={handleLikePost}
+            onMouseEnter={() => setIsHovered('like')}
+            onMouseLeave={() => setIsHovered('')}
+          >
+            <Image src={heartIcon} width={20} height={20} alt='like' />
+            <p className={isLiked ? 'text-[#E20A29]' : 'text-[#555555] text-[14px]'}>
+              {(updatedLikeCount ?? post.postDTO.likeCount) < 1000
+                ? updatedLikeCount ?? post.postDTO.likeCount
+                : '999+'}
+            </p>
           </div>
         </div>
       )}
